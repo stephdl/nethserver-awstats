7efd65a1906d26e6d056b11f2bf7c8c12e4afb89
Stephane de Labrusse <stephdl@de-labrusse.fr>
2017-05-27 20:51:00 +0200

    translation and transiflex

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Awstats.php
@@ -0,0 +1,15 @@
+<?php
+$L['Awstats_Title'] = 'Awstats Statistic';
+$L['Settings_Title'] = 'Settings';
+$L['Statistics_Title'] = 'Statistics';
+$L['AwstatsStatistics_Title'] = 'Consult Awstats Statistics';
+$L['VirtualhostsStatistics_label'] = 'Virtualhost statistics';
+$L['Virtualhosts_label'] = 'Virtualhosts';
+$L['AwstatsDomainURL_label'] = 'Domains';
+$L['status_label'] = 'Status';
+$L['access_private_label'] = 'Private Access';
+$L['access_public_label'] = 'Public Access';
+$L['Minute_label'] = 'Run Awstats each minute';
+$L['Users_label'] = 'Allowed users to consult awstats pages';
+$L['AwstatsSettings_header'] = 'Settings of the logfile analyser';
+$L['AwstatDisabledByProp_label'] = 'Awstats is disabled';
